f16fd32639b727ed3673e5e92303c932ba27d932
Lokesh Kumawat <lokesh8250kumawat@gmail.com>
2024-12-23 19:38:18 +0530

    Updated: Backend server port changed

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8001/api', // Laravel API base URL
+    baseURL: 'http://127.0.0.1:8000/api', // Laravel API base URL
 });
 
 export default axiosInstance;
