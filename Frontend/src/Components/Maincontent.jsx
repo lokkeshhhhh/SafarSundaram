4fd3a25caf6de093cb5034ccac1054577ace673d
Lokesh Kumawat <lokesh8250kumawat@gmail.com>
2025-01-01 14:23:56 +0530

    Fixed: Unnecessary footer links and text.

--- Frontend/src/Components/Maincontent.jsx
+++ Frontend/src/Components/Maincontent.jsx
@@ -880,13 +880,6 @@ const Maincontent = () => {
           </div>
           <div className="container copyright text-center mt-4">
             <p>© <span>Copyright</span> <strong className="px-1 sitename">Bell</strong> <span>All Rights Reserved</span></p>
-            <div className="credits">
-              All the links in the footer should remain intact.
-              You can delete the links only if you've purchased the pro version.
-              Licensing information: https://bootstrapmade.com/license/
-              Purchase the pro version with working PHP/AJAX contact form: [buy-url]
-              Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-            </div>
           </div>
         </footer>
         {/* Scroll Top */}
