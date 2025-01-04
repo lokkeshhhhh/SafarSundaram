ead3f1d5643a7da2e8ca134617f4e57c123c5da8
Lokesh Kumawat <lokesh8250kumawat@gmail.com>
2025-01-04 17:29:58 +0530

    Updated: Moved footer to Body directory

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import Maincontent from "./components/Maincontent";
 import Header from "./Body/Header";
+import Footer from "./Body/Footer";
 function App() {
     return (
         <>
             <Header />
             <Maincontent />
+            <Footer />
         </>
     )
 }
--- Frontend/src/Components/Maincontent.jsx
+++ Frontend/src/Components/Maincontent.jsx
@@ -774,53 +774,6 @@ const Maincontent = () => {
           </section>
         </main>
 
-        <footer id="footer" className="footer dark-background">
-          <div className="container">
-            <div className="row gy-3">
-              <div className="col-lg-3 col-md-6 d-flex">
-                <i className="bi bi-geo-alt icon"></i>
-                <div className="address">
-                  <h4>Address</h4>
-                  <p>A108 Adam Street</p>
-                  <p>New York, NY 535022</p>
-                  <p></p>
-                </div>
-              </div>
-              <div className="col-lg-3 col-md-6 d-flex">
-                <i className="bi bi-telephone icon"></i>
-                <div>
-                  <h4>Contact</h4>
-                  <p>
-                    <strong>Phone:</strong> <span>+1 5589 55488 55</span><br />
-                    <strong>Email:</strong> <span>info@example.com</span><br />
-                  </p>
-                </div>
-              </div>
-              <div className="col-lg-3 col-md-6 d-flex">
-                <i className="bi bi-clock icon"></i>
-                <div>
-                  <h4>Opening Hours</h4>
-                  <p>
-                    <strong>Mon-Sat:</strong> <span>11AM - 23PM</span><br />
-                    <strong>Sunday</strong>: <span>Closed</span>
-                  </p>
-                </div>
-              </div>
-              <div className="col-lg-3 col-md-6">
-                <h4>Follow Us</h4>
-                <div className="social-links d-flex">
-                  <a href="#" className="twitter"><i className="bi bi-twitter-x"></i></a>
-                  <a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
-                  <a href="#" className="instagram"><i className="bi bi-instagram"></i></a>
-                  <a href="#" className="linkedin"><i className="bi bi-linkedin"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="container copyright text-center mt-4">
-            <p>© <span>Copyright</span> <strong className="px-1 sitename">Bell</strong> <span>All Rights Reserved</span></p>
-          </div>
-        </footer>
         {/* Scroll Top */}
         <a href="#" id="scroll-top" className="scroll-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short"></i></a>
         {/* Preloader */}
