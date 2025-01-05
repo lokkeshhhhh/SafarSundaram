a4c2cd707d7ef668bc2583f74754dfca4c8b6ab5
Lokesh Kumawat <lokesh8250kumawat@gmail.com>
2025-01-05 12:14:38 +0530

    Updated: Applications turned route based

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Maincontent from "./Body/Main_Frontent/Maincontent";
 import Header from "./Body/Main_Frontent/Header";
 import Footer from "./Body/Main_Frontent/Footer";
 function App() {
     return (
-        <>
-            <Header />
-            <Maincontent />
-            <Footer />
-        </>
+        // <>
+        //     <Header />
+        //     <Maincontent />
+        //     <Footer />
+        // </>
+
+        <Router basename="/SafarSundaram">
+            <Routes>
+                {/* Main Website Routes */}
+                <Route path="/" element={
+                    <>
+                        <Header />
+                        <Maincontent />
+                        <Footer />
+                    </>
+                } />
+                {/* <Route path="/services" element={<MainLayout><Services /></MainLayout>} /> */}
+                {/* <Route path="/about" element={<MainLayout><About /></MainLayout>} /> */}
+                {/* <Route path="/contact" element={<MainLayout><Contact /></MainLayout>} /> */}
+            </Routes>
+        </Router>
     )
 }
 
